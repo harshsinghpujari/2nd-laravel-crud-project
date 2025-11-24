f91dd1d8315971099062b7911ab5185bf93a17e8
Himanshu Singh <himanshusingh83683@gmail.com>
2025-11-24 18:16:10 +0530

    Add Post model and establish user-post relationship in User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,4 +45,8 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function userCoolPosts() {
+        return $this->hasMany(Post::class, 'user_id');
+    }
 }
