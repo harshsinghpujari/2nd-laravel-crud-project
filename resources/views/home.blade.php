36fdbe6d9301a2c731f04d20b97722b973b9b0a5
Himanshu Singh <himanshusingh83683@gmail.com>
2025-11-24 18:26:40 +0530

    Add edit post view and enhance home view with post creation and listing

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,18 +13,59 @@
       @csrf
       <button>logout</button>
     </form>
+
+    <div style="border: 2px solid black">
+      <h2>Create a New Post</h2>
+    
+      <form action="/create-post" method="POST">
+        @csrf
+        <input type="text" name="title">
+        <textarea name="body" placeholder="body content..."></textarea>
+        <button>Save Post</button>
+      </form>
+    </div>
+
+      <div style="border: 2px solid black">
+        <h2>All Posts</h2>
+        @foreach ($posts as $post)
+        <div style="background-color:gray; padding: 10px; margin: 10px;">
+          <h3> {{$post['title']}} by {{$post->user->name}} </h3>
+           {{$post['body']}}
+           <p><a href="/edit-post/{{$post -> id}}">Edit</a></p>
+           <form action="/delete-post/{{$post->id}}"  method="POST">
+            @csrf
+            @method('DELETE')
+            <button>DELETE</button>
+          </form>
+        </div>            
+        @endforeach
+      </div>
       
   @else
-    
+    <div style="border:2px solid black">
+      <h2>Register</h2>
+      <form action="/register" method="POST">
+        @csrf
+      <input name="name" type="text" placeholder="name">
+      <input name="email" type="text" placeholder="email">
+      <input name="password" type="password" placeholder="password">
+      <button>Register</button>
+      </form>
+    </div>
+
+    <div style="border:2px solid black">
+      <h2>Login</h2>
+      <form action="/login" method="POST">
+        @csrf
+      <input name="loginname" type="text" placeholder="name">
+      <input name="loginpassword" type="password" placeholder="password">
+      <button>Login</button>
+      </form>
+    </div>
+
+    @endauth
+ 
 
 
-  <form action="/register" method="POST">
-
-    @csrf
-    <input name="name" type="text" placeholder="name">
-    <input name="email" type="text" placeholder="email">
-    <input name="password" type="password" placeholder="password">
-    <button>Register</button>
-  </form>
 </body>
 </html>
